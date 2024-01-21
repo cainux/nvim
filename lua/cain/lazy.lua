fcef65ede2f30e0813ed79a694f7f24e1d9a655c
Cain Voong <cainux@gmail.com>
2024-01-21 20:11:49 +0000

    Some LSP stuff

--- lua/cain/lazy.lua
+++ lua/cain/lazy.lua
@@ -64,16 +64,16 @@ require('lazy').setup({
     { 'williamboman/mason-lspconfig.nvim' },
 
     -- lsp-zero 3.x
-    -- { 'VonHeikemen/lsp-zero.nvim', branch = 'v3.x' },
-    -- { 'neovim/nvim-lspconfig' },
-    -- { 'hrsh7th/cmp-nvim-lsp' },
-    -- { 'hrsh7th/nvim-cmp' },
-    -- { 'L3MON4D3/LuaSnip' },
+    { 'VonHeikemen/lsp-zero.nvim', branch = 'v3.x' },
+    { 'neovim/nvim-lspconfig' },
+    { 'hrsh7th/cmp-nvim-lsp' },
+    { 'hrsh7th/nvim-cmp' },
+    { 'L3MON4D3/LuaSnip' },
 
     -- Stuff from ThePrimagens 0 to LSP
-    -- { 'hrsh7th/cmp-buffer' },
-    -- { 'hrsh7th/cmp-path' },
-    -- { 'saadparwaiz1/cmp_luasnip' },
-    -- { 'hrsh7th/cmp-nvim-lua' },
-    -- { 'rafamadriz/friendly-snippets' },
+    { 'hrsh7th/cmp-buffer' },
+    { 'hrsh7th/cmp-path' },
+    { 'saadparwaiz1/cmp_luasnip' },
+    { 'hrsh7th/cmp-nvim-lua' },
+    { 'rafamadriz/friendly-snippets' },
 })
