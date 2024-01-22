ca0415534aeaf41ac397b3d834eb440e64cb4724
Cain Voong <cainux@gmail.com>
2024-01-22 22:42:00 +0000

    More sets

--- lua/cain/set.lua
+++ lua/cain/set.lua
@@ -8,6 +8,7 @@ vim.opt.relativenumber = true
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 -- vim.opt.isfname:append("@-@")
+vim.opt.colorcolumn = "80"
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
@@ -16,4 +17,11 @@ vim.opt.expandtab = true
 
 vim.opt.smartindent = true
 
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
 vim.opt.wrap = false
+
+vim.opt.termguicolors = true
+vim.opt.updatetime = 50
+
