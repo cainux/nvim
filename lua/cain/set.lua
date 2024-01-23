8631a1cb51c5e9b10c2de1300074393e847d1e09
Cain Voong <cainux@gmail.com>
2024-01-23 18:45:40 +0000

    Widened the color column

--- lua/cain/set.lua
+++ lua/cain/set.lua
@@ -8,7 +8,7 @@ vim.opt.relativenumber = true
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 -- vim.opt.isfname:append("@-@")
-vim.opt.colorcolumn = "80"
+vim.opt.colorcolumn = "160"
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
