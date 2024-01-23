a908a5e335fa54769e96c2125129a27c68415556
Cain Voong <cainux@gmail.com>
2024-01-23 22:13:47 +0000

    Some very basic debug going...

--- lua/cain/lazy.lua
+++ lua/cain/lazy.lua
@@ -1,23 +1,23 @@
 -- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
     vim.fn.system({
-        'git',
-        'clone',
-        '--filter=blob:none',
-        'https://github.com/folke/lazy.nvim.git',
-        '--branch=stable', -- latest stable release
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
         lazypath,
     })
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Setup Plugins
-require('lazy').setup({
+require("lazy").setup({
 
     -- This seems like a cool theme
     {
-        'folke/tokyonight.nvim',
+        "folke/tokyonight.nvim",
         lazy = false,
         priority = 1000,
         opts = {},
@@ -25,28 +25,28 @@ require('lazy').setup({
 
     -- Search for and open file
     {
-        'nvim-telescope/telescope.nvim', tag = '0.1.5',
-        dependencies = { 'nvim-lua/plenary.nvim' }
+        "nvim-telescope/telescope.nvim", tag = "0.1.5",
+        dependencies = { "nvim-lua/plenary.nvim" }
     },
 
     -- Syntax highlighting
     {
-        'nvim-treesitter/nvim-treesitter', build = ':TSUpdate',
+        "nvim-treesitter/nvim-treesitter", build = ":TSUpdate",
         config = function()
-            local configs = require('nvim-treesitter.configs')
+            local configs = require("nvim-treesitter.configs")
 
             configs.setup({
                 ensure_installed = {
-                    'lua',
-                    'vim',
-                    'vimdoc',
-                    'query',
-                    'hcl',
-                    'c_sharp',
-                    'javascript',
-                    'typescript',
-                    'yaml',
-                    'dockerfile'
+                    "lua",
+                    "vim",
+                    "vimdoc",
+                    "query",
+                    "hcl",
+                    "c_sharp",
+                    "javascript",
+                    "typescript",
+                    "yaml",
+                    "dockerfile"
                 },
                 sync_install = false,
                 highlight = { enable = true },
@@ -55,25 +55,29 @@ require('lazy').setup({
         end
     },
 
-    { 'ThePrimeagen/harpoon' },
+    { "ThePrimeagen/harpoon" },
 
-    { 'f-person/git-blame.nvim' },
+    { "f-person/git-blame.nvim" },
 
     -- Manage language servers from within neovim
-    { 'williamboman/mason.nvim' },
-    { 'williamboman/mason-lspconfig.nvim' },
+    { "williamboman/mason.nvim" },
+    { "williamboman/mason-lspconfig.nvim" },
 
     -- lsp-zero 3.x
-    { 'VonHeikemen/lsp-zero.nvim', branch = 'v3.x' },
-    { 'neovim/nvim-lspconfig' },
-    { 'hrsh7th/cmp-nvim-lsp' },
-    { 'hrsh7th/nvim-cmp' },
-    { 'L3MON4D3/LuaSnip' },
+    { "VonHeikemen/lsp-zero.nvim", branch = "v3.x" },
+    { "neovim/nvim-lspconfig" },
+    { "hrsh7th/cmp-nvim-lsp" },
+    { "hrsh7th/nvim-cmp" },
+    { "L3MON4D3/LuaSnip" },
 
     -- Stuff from ThePrimagens 0 to LSP
-    { 'hrsh7th/cmp-buffer' },
-    { 'hrsh7th/cmp-path' },
-    { 'saadparwaiz1/cmp_luasnip' },
-    { 'hrsh7th/cmp-nvim-lua' },
-    { 'rafamadriz/friendly-snippets' },
+    { "hrsh7th/cmp-buffer" },
+    { "hrsh7th/cmp-path" },
+    { "saadparwaiz1/cmp_luasnip" },
+    { "hrsh7th/cmp-nvim-lua" },
+    { "rafamadriz/friendly-snippets" },
+
+    -- Debugging
+    { "mfussenegger/nvim-dap" },
+    { "rcarriga/nvim-dap-ui" },
 })
