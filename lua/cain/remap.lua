dd50b079a94f97f4688947805d4b1b3526020710
cainux <cainux@gmail.com>
2024-02-03 23:31:10 +0000

    Some more traditional shortcuts for me

--- lua/cain/remap.lua
+++ lua/cain/remap.lua
@@ -7,3 +7,9 @@ vim.keymap.set("n", "<leader>p", [["+p]])
 -- Copy to system clipboard
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+-- Not sure I'll get used to this
+vim.keymap.set("i", "<C-c>", "<Esc>")
+
+-- This is decades old for me
+vim.keymap.set("n", "<C-s>", ":update<CR>")
