944d4841eee2c99b92e4c3e1e98b2a1c404204f2
Cain Voong <cainux@gmail.com>
2024-04-16 19:04:20 +0100

    Autotag

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,8 @@
 return {
     "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+        "windwp/nvim-ts-autotag",
+    },
     build = ":TSUpdate",
     config = function()
         local configs = require "nvim-treesitter.configs"
@@ -22,6 +25,7 @@ return {
             sync_install = false,
             highlight = { enable = true },
             indent = { enable = true },
+            autotag = { enable = true },
         }
     end,
 }
